8c0753c7ec060175df1c5da58211e1e07f69cc47
Gabriel Alexandre <gabrielalexandrerabelo@gmail.com>
2025-02-09 10:20:24 -0300

    Added controlled inputs and validations

--- src/components/inputs.jsx
+++ src/components/inputs.jsx
@@ -1,6 +1,6 @@
 import styles from '../styles/input.module.css';
 
-export default function Input({ iconURL, iconDescription, type = 'text', placeholder }) {
+export default function Input({ iconURL, iconDescription, type = 'text', placeholder, required, value, onChange }) {
   return (
     <div className={`${styles.input} flex`}>
       <img 
@@ -11,6 +11,9 @@ export default function Input({ iconURL, iconDescription, type = 'text', placeho
       <input 
         type={type} 
         placeholder={placeholder} 
+        required={required}
+        value={value}
+        onChange={(e) => onChange(e.target.value)}
       />
     </div>
   );
--- src/screen/loginSignup.jsx
+++ src/screen/loginSignup.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
-import email from '../assets/email.png';
-import person from '../assets/person.png';
-import password from '../assets/password.png';
+import emailImage from '../assets/email.png';
+import personImage from '../assets/person.png';
+import passwordImage from '../assets/password.png';
 import Input from '../components/inputs';
 import styles from '../styles/loginSignup.module.css';
 import Button from '../components/button';
@@ -13,41 +13,94 @@ const btnGrayStyle = {
 };
 
 export default function LoginSignup() {
-  const [formVersion, setFormVersion] = useState('SignUp')
+  const [formVersion, setFormVersion] = useState('SignUp');
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
   // Actions
-  const handleFormToLogin = () => setFormVersion('Login');
-  const handleFormToSignUp = () => setFormVersion('SignUp');
+  const handleResetInput = () => {
+    setUsername('');
+    setEmail('');
+    setPassword('');
+  }
+  const handleFormToLogin = (e) => {
+    /**
+     * Prevents Submit from being activated when changing form versions 
+     * and clears values ​​previously placed in the input
+     */
+    formVersion !== 'Login' 
+      && e.preventDefault() || handleResetInput();
+
+    setFormVersion('Login');
+  }
+  const handleFormToSignUp = (e) => {
+    /**
+     * Prevents Submit from being activated when changing form versions 
+     * and clears values ​​previously placed in the input
+     */
+    formVersion !== 'SignUp' 
+      && e.preventDefault() || handleResetInput();
+    
+    setFormVersion('SignUp');
+  }     
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+
+    // Prevents sending if the email and password are empty
+    if(!email) return alert('Email must be completed');
+    if(!password) return alert('Password must be completed');
+
+    let dataForm = { email, password }
+    
+    if(formVersion === 'SignUp') {
+      // Prevents sending if the username are empty
+      if(!username) return alert('Name must be completed');
+
+      return console.log({...dataForm, username})
+    } 
+                                              
+    return console.log(dataForm)
+  }
 
   // Styles
   const styleBtnGray = (cond) => formVersion === cond ? btnGrayStyle : null;
 
   // Render
   const renderInputName = () => formVersion === 'SignUp' && <Input
-    iconURL={person}
+    iconURL={personImage}
     iconDescription='Person'
     placeholder='Name'
+    required
+    value={username}
+    onChange={setUsername}
   />
 
   return (
-    <form className={`${styles.container} flexCol`}>
+    <form className={`${styles.container} flexCol`} onSubmit={handleFormSubmit}>
       <h2 className={styles.formTitle}> {formVersion} </h2>
 
       <div className={`${styles.inputs} flexCol`}>
         {renderInputName()}
 
         <Input
-          iconURL={email}
+          iconURL={emailImage}
           iconDescription='Email'
           type='email'
           placeholder='Email'
+          required
+          value={email}
+          onChange={setEmail}
         />
 
         <Input
-          iconURL={password}
+          iconURL={passwordImage}
           iconDescription='Password locker'
           type='password'
           placeholder='Password'
+          required
+          value={password}
+          onChange={setPassword}
         />
       </div>
 
@@ -56,13 +109,15 @@ export default function LoginSignup() {
       </div>
 
       <div className={`${styles.submitContainer} flex`}>
-        <Button 
+        <Button
+          type='submit' 
           customStyle={styleBtnGray('Login')} 
           onClick={handleFormToSignUp}>
           Sign Up
         </Button>
         
-        <Button 
+        <Button
+          type='submit' 
           customStyle={styleBtnGray('SignUp')} 
           onClick={handleFormToLogin}>
           Login
